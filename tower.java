59bb0775dc0fea227d9e8e62ca8b3494b7aa406f
mileszwicky <miles.zwicky@gmail.com>
2017-11-18 11:58:32 +0100

    Initial Commit

---
+++ tower.java
@@ -0,0 +1,11 @@
+public class tower{
+    private HanoiStack rodA;
+    private HanoiStack rodB;
+    private HanoiStack rodC; 
+
+    tower(){
+        rodA = new HanoiStack();
+        rodB = new HanoiStack();
+        rodC = new HanoiStack();
+    }
+}
